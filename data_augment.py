19243838c09b36f09f4870010f4f73841b047911
Edward Lee <david1998zzz@gmail.com>
2019-05-08 11:21:38 +0800

    UPDATE data_augment.py

--- data_augment.py
+++ data_augment.py
@@ -1,16 +1,84 @@
 """
   library:
-  * PIL     pip3 install pillow
-  * scipy   pip3 install scipy
+  * PIL == 5.4.1    pip3 install pillow
+  * scipy           pip3 install scipy
 """
 
-import os
-import numpy as np
-from PIL import Image
 import argparse
+import os
+import sys
+import copy
+
+import random
+import numpy as np
+from joblib import Parallel, delayed
+from PIL import Image
 from scipy.misc import imsave
 from scipy.ndimage import rotate
-from joblib import Parallel, delayed
+
+def random_augments(sp):
+    print("Process %s" % sp)
+
+    count_im = 0
+    img_hazy = os.path.join(args.hazy, sp)
+    img_gt   = os.path.join(args.gt, '_'.join([sp.split('_')[0], sp.split('_')[1], 'GT' + '.' + sp.split('_')[-1].split('.')[-1]]))
+
+    raw_im_A, raw_im_B = Image.open(img_hazy), Image.open(img_gt)
+
+    h, w, _ = raw_im_A.shape
+    l = min(h, w)
+
+    # -----------------------------
+    # Random crop from any coordinate
+    #   Size: Random choice from list
+    #   X: Uniform Choose
+    #   Y: Uniform Choose
+    #   Flip and 
+    # -----------------------------
+    for _ in range(args.location):
+        # Random choose size
+        if l < 1024:   
+            size = 512
+        elif l < 1536:      
+            size = np.random.choice([512, 1024], size=None, replace=False, p=[0.6, 0.4])
+        else:           
+            size = np.random.choice([512, 1024, 1536], size=None, replace=False, p=[0.6, 0.3, 0.1]) 
+
+        x = random.randint(0, h - size)
+        y = random.randint(0, w - size)
+        
+        im_A = raw_im_A.crop((x, y, x+size, y+size)).resize((512, 512), Image.BILINEAR) # Hazy images
+        im_B = raw_im_B.crop((x, y, x+size, y+size)).resize((512, 512), Image.BILINEAR) # Clear images
+        
+        flip = random.choice([0, 1, 2])
+        degree = random.choice([0, 1, 2, 3])
+
+        # Horizontal Flip: transforms.HorizontalFlip()
+        # Vertial Flip: transforms.VerticalFlip()
+        if flip == 1:
+            im_A = im_A.transpose(Image.FLIP_LEFT_RIGHT)
+            im_B = im_B.transpose(Image.FLIP_LEFT_RIGHT)
+        elif flip == 2:
+            im_A = im_A.transpose(Image.FLIP_TOP_BOTTOM)
+            im_B = im_B.transpose(Image.FLIP_TOP_BOTTOM)
+
+        # Rotation: transforms.RandomRotation()
+        if degree == 1:
+            im_A = im_A.transpose(Image.ROTATE_90)
+            im_B = im_B.transpose(Image.ROTATE_90)
+        elif degree == 2:
+            im_A = im_A.transpose(Image.ROTATE_180)
+            im_B = im_B.transpose(Image.ROTATE_180)
+        elif degree == 3:
+            im_A = im_A.transpose(Image.ROTATE_270)
+            im_B = im_B.transpose(Image.ROTATE_270)
+
+        imsave(os.path.join(args.output, "hazy", str(count_im).zfill(4) + "_" + "_".join(sp.split('_')[:-1]) + ".png"), im_A)
+        imsave(os.path.join(args.output, "gt", str(count_im).zfill(4) + "_" + "_".join(sp.split('_')[:-1]) + ".png"), im_B)
+                
+        count_im += 1
+    
+    print("Process %s for %d" % (sp, count_im))
 
 def augments(sp):
     print("Process %s" % sp)
@@ -19,27 +87,30 @@ def augments(sp):
     img_hazy = os.path.join(args.hazy, sp)
     img_gt   = os.path.join(args.gt, '_'.join([sp.split('_')[0], sp.split('_')[1], 'GT' + '.' + sp.split('_')[-1].split('.')[-1]]))
 
+    raw_im_A = np.asarray(Image.open(img_hazy))
+    raw_im_B = np.asarray(Image.open(img_gt))
+
     for flip in [0, 1, 2]:
         for degree in [0, 1, 2, 3]:
+            im_A = raw_im_A.copy()
+            im_B = raw_im_B.copy()
 
-            im_A = np.asarray(Image.open(img_hazy))
-            im_B = np.asarray(Image.open(img_gt))
-
-            # Horizontal / Vertial Flip
+            # Horizontal Flip: transforms.HorizontalFlip()
             if flip == 1:
                 im_A = np.flip(im_A, 0)
                 im_B = np.flip(im_B, 0)
+            # Vertial Flip: transforms.VerticalFlip()
             if flip == 2:
                 im_A = np.flip(im_A, 1)
                 im_B = np.flip(im_B, 1)
 
-            # Rotation
+            # Rotation: transforms.RandomRotation()
             if degree != 0:
                 im_A = rotate(im_A, 90 * degree)
                 im_B = rotate(im_B, 90 * degree)
 
             # Height, Width, Channel
-            h, w, c = im_A.shape
+            h, w, _ = im_A.shape
 
             for x in range(0, h, args.stride):
                 for y in range(0, w, args.stride):
@@ -50,21 +121,23 @@ def augments(sp):
 
                         imsave(os.path.join(args.output, "hazy", str(count_im).zfill(4) + "_" + "_".join(sp.split('_')[:-1]) + ".png"), patch_A)
                         imsave(os.path.join(args.output, "gt", str(count_im).zfill(4) + "_" + "_".join(sp.split('_')[:-1]) + ".png"), patch_B)
-                        # imsave("%s/data/%d_%s.png" % (args.output, str(count_im).zfill(4), '_'.join(sp.split('_')[:-1])), patch_A)
-                        # imsave("%s/label/%d_%s.png" % (args.output, str(count_im).zfill(4), '_'.join(sp.split('_')[:-1])), patch_B)
-
+                        
                         count_im += 1
 
     print("Process %s for %d" % (sp, count_im))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Data augmentation: Create image pairs')
-    parser.add_argument("--size", type=int, default=512, help="which size to generate")
+    parser.add_argument("--location", type=int, default=5000, help="how many location coordinate to sample")
+    parser.add_argument("--size", type=int, default=512, help="the size when cropping images")
     parser.add_argument("--stride", type=int, default=256, help="the stride when cropping images")
-    subparser  = parser.add_subparsers(required=True, dest="command")
+    parser.add_argument("--random", action="store_true")
+    parser.add_argument("--step", action="store_true")
 
+    domainparser  = parser.add_subparsers(required=True, dest="domain")
+    
     # I-Haze
-    ihazeparser = subparser.add_parser("I-Haze")
+    ihazeparser = domainparser.add_parser("I-Haze")
     ihazeparser.add_argument('--hazy', help='Input directory for Hazy Image', type=str,
                             default="/media/disk1/EdwardLee/dataset/IndoorTrainHazy")
     ihazeparser.add_argument('--gt', help='Input directory for Clear Image', type=str,
@@ -73,41 +146,62 @@ if __name__ == "__main__":
                             default="/media/disk1/EdwardLee/IndoorTrain")
 
     # O-Haze
-    ohazeparser = subparser.add_parser("O-Haze")
+    ohazeparser = domainparser.add_parser("O-Haze")
     ohazeparser.add_argument('--hazy', help='Input directory for Hazy Image', type=str,
                             default="/media/disk1/EdwardLee/dataset/OutdoorTrainHazy")
     ohazeparser.add_argument('--gt', help='Input directory for Clear Image', type=str,
                             default="/media/disk1/EdwardLee/dataset/OutdoorTrainGT")
     ohazeparser.add_argument('--output', help='Output directory', type=str,
                             default="/media/disk1/EdwardLee/OutdoorTrain")
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     print(args)
 
-    splits = os.listdir(args.hazy)
-
+    # ----------------------------------------------------------
     # Check folders here, make the directories if don't exist.
+    # 0.  The image
     # 1.  Root folder
     #     1.1 gt
-    #     2.2 hazy
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
-
-    if not os.path.exists(os.path.join(args.output, "gt")):
-        os.makedirs(os.path.join(args.output, "gt"))
-    else:
-        os.system("rm " + os.path.join(args.output, "gt", "*"))
-
-    if not os.path.exists(os.path.join(args.output, "hazy")):
-        os.makedirs(os.path.join(args.output, "hazy"))
-    else:
-        os.system("rm " + os.path.join(args.output, "hazy", "*"))
-
+    #     1.2 hazy
+    # ---------------------------------------------------------
     if not os.path.exists(args.hazy):
         raise IOError("File doesn't not exist: {}".format(args.hazy))
 
     if not os.path.exists(args.gt):
         raise IOError("File doesn't not exist: {}".format(args.gt))
 
+    # 1 Root Folder
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    # 1.1 Ground Truth
+    if not os.path.exists(os.path.join(args.output, "gt")):
+        os.makedirs(os.path.join(args.output, "gt"))
+    else:
+        print("Clean the folder: {}".format(os.path.join(args.output, "gt")))
+        command = input("Clean the folder? [Y/N]")
+        
+        if command == 'Y':
+            print("Clean {}.".format(os.path.join(args.output, "gt")))
+            os.system("rm " + os.path.join(args.output, "gt", "*"))
+        
+    # 1.2 Hazy Images
+    if not os.path.exists(os.path.join(args.output, "hazy")):
+        os.makedirs(os.path.join(args.output, "hazy"))
+    else:
+        print("Clean the folder: {}".format(os.path.join(args.output, "hazy")))
+        command = input("Y/N")
+        
+        if command == 'Y':
+            print("Clean {}".format(os.path.join(args.output, "hazy")))
+            os.system("rm " + os.path.join(args.output, "hazy", "*"))
+        
     # Function parallel working...
-    Parallel(-1)(delayed(augments)(sp) for sp in splits)
+    splits = os.listdir(args.hazy)
+
+    if args.step:
+        print("Executing: augments")
+        Parallel(-1)(delayed(augments)(sp) for sp in splits)
+    elif args.random:
+        print("Executing: random_augments")
+        Parallel(-1)(delayed(random_augments)(sp) for sp in splits)
