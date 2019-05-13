ba442ebbe2ba1fa8f50ecfb1e38105e411ec5d2c
Yu-Wei Chang <b01505025@g.ntu.edu.tw>
2019-05-13 12:18:23 +0800

    MODIFY feature.py

--- feature.py
+++ feature.py
@@ -10,6 +10,10 @@ import torchvision.models as models
 from torch import nn, optim
 from torch.backends import cudnn
 from torch.utils.data import DataLoader
+from torch.utils import model_zoo
+
+__all__ = []
+model_urls = {}
 
 import utils
 from data import DatasetFromFolder
@@ -17,11 +21,11 @@ from model.rpnet import Net
 
 
 def main():
-    vgg16_bn = models.vgg16_bn(pretrained=True, progress=True)
-    resnet   = models.resnet18(pretrained=True, progress=True)
+    vgg16_bn = models.vgg16_bn(pretrained=True)
+    resnet   = models.resnet18(pretrained=True)
 
-    print(vgg16_bn)
-    print(resnet)
+    print(vgg16_bn.features)
+    # print(resnet)
 
 if __name__ == "__main__":
     main()
