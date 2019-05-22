df5be95190233f0e325cd5d6d087f01b640c9a52
Edward Lee <david1998zzz@gmail.com>
2019-05-22 20:21:01 +0800

    MODIFY train.py

--- train.py
+++ train.py
@@ -11,52 +11,56 @@ import os
 import pprint
 from datetime import date
 
-from tqdm import tqdm
-
 import numpy as np
+import tensorboardX
 import torch
+import torchsummary
 import torchvision
 from matplotlib import pyplot as plt
 from skimage.measure import compare_psnr, compare_ssim
+from tensorboardX import SummaryWriter
 from torch import nn, optim
 from torch.backends import cudnn
 from torch.utils.data import DataLoader
 from torchvision.transforms import (
     CenterCrop, Compose, Normalize, RandomCrop, Resize, ToTensor)
-# from tensorboardX import SummaryWriter
-import torchsummary
 from torchvision.utils import make_grid
+from tqdm import tqdm
 
 import utils
 from data import DatasetFromFolder
 from model.rpnet import Net
 
-# Training settings
 parser = argparse.ArgumentParser(description="PyTorch DeepDehazing")
-parser.add_argument("--tag", type=str, default="Indoor_512", help="tag for this training")
+# Basic Training settings
 parser.add_argument("--rb", type=int, default=12, help="number of residual blocks")
 parser.add_argument("--batchSize", type=int, default=16, help="training batch size")
 parser.add_argument("--nEpochs", type=int, default=30, help="number of epochs to train for")
 parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
-parser.add_argument("--step", type=int, default=1000, help="step to test the model performance")
-parser.add_argument("--cuda", default=True, help="Use cuda?")
+parser.add_argument("--activation", default="LeakyReLU", help="the activation function use at training")
 parser.add_argument("--normalize", default=False, action="store_true", help="normalized the dataset images")
-parser.add_argument("--gpus", type=int, default=1, help="nums of gpu to use")
-parser.add_argument("--resume", type=str, help="Path to checkpoint (default: none)")
 parser.add_argument("--milestones", type=int, nargs='*', default=[10], help="Which epoch to decay the learning rate")
 parser.add_argument("--gamma", type=float, default=0.1, help="The ratio of decaying learning rate everytime")
 parser.add_argument("--start_epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
-parser.add_argument("--threads", type=int, default=8, help="Number of threads for data loader to use, Default: 1")
 parser.add_argument("--momentum", default=0.9, type=float, help="SGD Momentum, Default: 0.9")
 parser.add_argument("--pretrained", type=str, help="path to pretrained model (default: none)")
-parser.add_argument("--train", default="/media/disk1/EdwardLee/IndoorTrain_512", type=str, help="path to load train datasets")
-parser.add_argument("--val", default="/media/disk1/EdwardLee/IndoorVal_512", type=str, help="path to load val datasets")
-# parser.add_argument("--test", default="/media/disk1/EdwardLee/IndoorTest", type=str, help="path to load test datasets")
+# Message logging, model saving setting
+parser.add_argument("--tag", type=str, default="Indoor_512", help="tag for this training")
 parser.add_argument("--checkpoints", default="/media/disk1/EdwardLee/checkpoints", type=str, help="path to save the checkpoints")
+parser.add_argument("--step", type=int, default=1000, help="step to test the model performance")
 parser.add_argument("--log_interval", type=int, default=10, help="interval per iterations to log the message")
 parser.add_argument("--save_interval", type=int, default=1, help="interval per epochs to save the model")
 parser.add_argument("--detail", default="./train_details", help="the root directory to save the training details")
-parser.add_argument("--activation", default="LeakyReLU", help="the activation function use at training")
+# Device setting
+parser.add_argument("--cuda", default=True, help="Use cuda?")
+parser.add_argument("--gpus", type=int, default=1, help="nums of gpu to use")
+parser.add_argument("--threads", type=int, default=8, help="Number of threads for data loader to use, Default: 1")
+parser.add_arugment("--fixrandomseed", default=False, help="train with fix random seed")
+# Dataset loading, pretrain model setting
+parser.add_argument("--resume", type=str, help="Path to checkpoint (default: none)")
+parser.add_argument("--train", default="/media/disk1/EdwardLee/IndoorTrain_512", type=str, help="path to load train datasets")
+parser.add_argument("--val", default="/media/disk1/EdwardLee/IndoorVal_512", type=str, help="path to load val datasets")
+
 # subparser = parser.add_subparsers(required=True, dest="command", help="I-Haze / O-Haze")
 
 # ihazeparser = subparser.add_parser("I-Haze")
@@ -69,27 +73,31 @@ parser.add_argument("--activation", default="LeakyReLU", help="the activation fu
 
 opt = parser.parse_args()
 
+# (Deprecated)
 # Set logger
-logging.config.fileConfig("logging.ini")
-statelogger = logging.getLogger(__name__)
+# logging.config.fileConfig("logging.ini")
+# statelogger = logging.getLogger(__name__)
 
 # Select Device
 device = utils.selectDevice()
 cudnn.benchmark = True
 
 def main():
-    global opt, name, model, criterion
+    global opt, name, model, criterion, writer
 
-    # Establish the folder
-    name = "{}_{}_{}_{}".format(opt.tag, date.today().strftime("%Y%m%d"), opt.rb, opt.batchSize)
-    # print("Name: {}".format(name))
-
+    # Establish the folder and the summarywriter
+    name    = "{}_{}_{}_{}".format(opt.tag, date.today().strftime("%Y%m%d"), opt.rb, opt.batchSize)
+    writer  = SummaryWriter("./{}/{}".format(opt.detail, name))
+    
     if opt.cuda and not torch.cuda.is_available():
         raise Exception("No GPU found, please run without --cuda")
 
-    # seed = 1334
-    # torch.manual_seed(seed)
-    # if opt.cuda: torch.cuda.manual_seed(seed)
+    if opt.fixrandomseed:
+        seed = 1334
+        torch.manual_seed(seed)
+        
+        if opt.cuda: 
+            torch.cuda.manual_seed(seed)
 
     print("==========> Loading datasets")
     # -----------------------------------------------------------------
@@ -107,12 +115,10 @@ def main():
     
     train_dataset = DatasetFromFolder(opt.train, transform=img_transform)
     val_dataset   = DatasetFromFolder(opt.val, transform=img_transform)
-    # test_dataset  = DatasetFromFolder(opt.test, transform=img_transform)
 
     train_loader = DataLoader(dataset=train_dataset, num_workers=opt.threads, batch_size=opt.batchSize, pin_memory=True, shuffle=True)
     val_loader   = DataLoader(dataset=val_dataset, num_workers=opt.threads, batch_size=opt.batchSize, pin_memory=True, shuffle=False)
-    # test_loader  = DataLoader(dataset=test_dataset, num_workers=opt.threads, batch_size=1, pin_memory=True, shuffle=False)
-
+    
     print("==========> Building model")
     model = Net(opt.rb)
     criterion = nn.MSELoss(size_average=True)
@@ -131,7 +137,6 @@ def main():
             model.load_state_dict(checkpoint["state_dict"])
         else:
             raise Exception("=> no checkpoint found at '{}'".format(opt.resume))
-            # print("=> no checkpoint found at '{}'".format(opt.resume))
 
     # optionally copy weights from a checkpoint
     if opt.pretrained:
@@ -141,7 +146,6 @@ def main():
             model.load_state_dict(weights['state_dict'].state_dict())
         else:
             raise Exception("=> no pretrained model found at '{}'".format(opt.pretrained))
-            # print("=> no model found at '{}'".format(opt.pretrained))
 
     if opt.cuda:
         print("==========> Setting GPU")
@@ -164,13 +168,13 @@ def main():
     iterations = np.empty(0, dtype=float)
 
     print("==========> Training setting")
-    details(opt, "./train_details/{}/{}".format(name, "train_setting.txt"))
+    details(opt, "./{}/{}/{}".format(opt.detail, name, "args.txt"))
 
     print("==========> Training")
     for epoch in range(opt.start_epoch, opt.nEpochs + 1):
         scheduler.step()
 
-        loss_iter, mse_iter, psnr_iter, ssim_iter, iterations = train(
+        loss_iter, mse_iter, psnr_iter, ssim_iter, iterations = train_eval(
             train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_iter, ssim_iter, iterations)
         
         # utils.save_checkpoint(model, opt.checkpoints, epoch, name)
@@ -178,9 +182,9 @@ def main():
 
     return
 
-def train(train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_iter, ssim_iter, iters):
-    statelogger.info("epoch: {}, lr: {}".format(epoch, optimizer.param_groups[0]["lr"]))
-
+def train_eval(train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_iter, ssim_iter, iters):
+    writer.add_scalar("learning_rate", optimizer.param_groups[0]["lr"], epoch)
+    
     trainLoss = []
 
     for iteration, (data, label) in enumerate(train_loader, 1):
@@ -206,10 +210,10 @@ def train(train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_
         # Log the training message, testing, saving the network
         # -----------------------------------------------------
         if steps % opt.log_interval == 0:
-            statelogger.info("===> Epoch[{}]({}/{}): Loss: {:.6f}".format(epoch, iteration, len(train_loader), loss.item()))
-            # logger.add_scalar('loss', loss.data[0], steps)
+            print("===> Epoch[{}]({}/{}): Loss: {:.6f}".format(epoch, iteration, len(train_loader), loss.item()))
+            writer.add_scalar('Train_loss', loss.data[0], steps)
 
-        if steps % opt.step == 0:
+        if steps % opt.save_interval == 0:
             data_temp   = make_grid(data.data, nrow=8)
             label_temp  = make_grid(label.data, nrow=8)
             output_temp = make_grid(output.data, nrow=8)
@@ -221,16 +225,18 @@ def train(train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_
             # In epoch testing and saving (Newly added)
             utils.save_checkpoint(model, opt.checkpoints, epoch, name, iteration)        
             
+        if steps % opt.step == 0:
             # mses, psnrs, ssims = test(val_loader, epoch, criterion)
-            mses, psnrs = test(val_loader, epoch, criterion)
+            mse, psnr = test(val_loader, epoch, criterion)
             loss_iter = np.append(loss_iter, np.array([np.mean(trainLoss)]), axis=0)
-            mse_iter  = np.append(mse_iter, np.array([mses]), axis=0)
-            psnr_iter = np.append(psnr_iter, np.array([psnrs]), axis=0)
-            # ssim_iter = np.append(ssim_iter, np.array([ssims]), axis=0)
+            mse_iter  = np.append(mse_iter, np.array([mse]), axis=0)
+            psnr_iter = np.append(psnr_iter, np.array([psnr]), axis=0)
+            # ssim_iter = np.append(ssim_iter, np.array([ssim]), axis=0)
             iters     = np.append(iters, np.array([steps / len(train_loader)]), axis=0)
 
             trainLoss = []  # Clean the list 
 
+            # (Deprecated)
             # mse_mean  = np.average(mse_iter, axis=1)
             # psnr_mean = np.average(psnr_iter, axis=1)
             # ssim_mean = np.average(ssim_iter, axis=1)
@@ -242,8 +248,10 @@ def train(train_loader, val_loader, optimizer, epoch, loss_iter, mse_iter, psnr_
             # ----------------------------------------------------------
             # Plot TrainLoss, TestLoss and the minimum value of TestLoss
             # ----------------------------------------------------------
-            draw_graphs(loss_iter, mse_iter, psnr_iter, ssim_iter, iters, len(train_loader))
-        
+            writer.add_scalar('Train_Loss', trainLoss, steps / len(train_loader))
+            writer.add_scalar('Val_Loss', mse, steps / len(train_loader))
+            writer.add_scalar('Val_PSNR', psnr, steps / len(train_loader))
+
     return loss_iter, mse_iter, psnr_iter, ssim_iter, iters
 
 def details(opt, path):
@@ -269,7 +277,7 @@ def details(opt, path):
     return folder
 
 def draw_graphs(train_loss, val_loss, psnr, ssim, x, iters_per_epoch, 
-            loss_filename="loss.png", loss_log_filename="loss_log.png", psnr_filename="psnr_ssim.png"):
+            loss_filename="loss.png", loss_log_filename="loss_log.png", psnr_filename="psnr.png"):
     # Linear scale of loss curve
     plt.clf()
     plt.figure(figsize=(12.8, 7.2))
@@ -320,6 +328,14 @@ def draw_graphs(train_loss, val_loss, psnr, ssim, x, iters_per_epoch,
     return
 
 def test(loader, epoch, criterion):
+    """
+      Params:
+      - loader
+
+      Return:
+      - np.mean(mse)
+      - np.mean(psnr)
+    """
     psnrs = []
     ssims = []
     mses = []
@@ -364,12 +380,12 @@ def test(loader, epoch, criterion):
         mse_mean  = np.mean(mses)
         # ssim_mean = np.mean(ssims)
 
-        statelogger.info("[Vaild] epoch: {}, mse: {}".format(epoch, mse_mean))
-        statelogger.info("[Vaild] epoch: {}, psnr: {}".format(epoch, psnr_mean))
-        # statelogger.info("[Vaild] epoch: {}, ssim: {}".format(epoch, ssim_mean))
+        print("[Vaild] epoch: {}, mse: {}".format(epoch, mse_mean))
+        print("[Vaild] epoch: {}, psnr: {}".format(epoch, psnr_mean))
+        # print("[Vaild] epoch: {}, ssim: {}".format(epoch, ssim_mean))
 
     return np.mean(mses), np.mean(psnrs) #, np.mean(ssims)
 
 if __name__ == "__main__":
-    os.system('clear')
+    os.system('clear')    
     main()
