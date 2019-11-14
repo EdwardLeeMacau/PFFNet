6b914c377536ecb3217c505ed3c337fc24e68ff9
EdwardLeeMacau <b05901119@ntu.edu.tw>
2019-11-14 23:21:12 +0800

    Modify tideScript.py

--- tideScript.py
+++ tideScript.py
@@ -8,10 +8,22 @@ def traverseLog(args):
 
     for tag in tags:
         fpath = os.path.join(args.log, tag, 'statistical.xlsx')
+    
         if os.path.exists(fpath):
-            df = pd.read_excel(fpath)
-            print(fpath, df.shape)
-            stat[tag] = df
+            df = pd.read_excel(fpath).drop_duplicates().astype(
+                {'Iterations': int}
+            ).set_index('Iterations').drop(
+                axis='columns', 
+                labels='Unnamed: 0'
+            )
+            stat[tag] = df.nsmallest(args.number, 'ValidationLoss', 'first')
+
+    if len(stat) > 0:
+        stat = pd.concat(
+            stat.values(), axis=0, keys=stat.keys(), names=['Tags']
+        )
+    else:
+        stat = pd.DataFrame()
 
     return stat 
 
@@ -21,23 +33,54 @@ def traverseOutput(args):
 
     for tag in tags:
         epochs = os.listdir(os.path.join(args.dir, tag))
+        
+        tmp = {}
         for epoch in epochs:
             fpath = os.path.join(os.path.join(args.dir, tag, epoch, './record.xlsx'))
-            if os.path.exists(fpath):
-                df = pd.read_excel(fpath)
-                print(fpath, df)
-                stat[(tag, epoch)] = df
 
+            if os.path.exists(fpath):
+                df = pd.read_excel(fpath).rename(
+                    columns={'Unnamed: 0': 'Standard'}
+                ).set_index('Standard')
+                tmp[epoch] = df
+
+        if len(tmp) > 0:
+            stat[tag] = pd.concat(
+                tmp.values(), axis=0, keys=tmp.keys(), names=['Iterations']
+            )
+
+    if len(stat) > 0:
+        stat = pd.concat(
+            stat.values(), axis=0, keys=stat.keys(), names=['Tags']
+        )
+    else:
+        stat = pd.DataFrame()
+    
     return stat
 
 def main(args):
-    traverseLog(args)
-    traverseOutput(args)
+    trainCurve  = traverseLog(args)
+    trainCurve.columns = pd.MultiIndex.from_product([['Train'], trainCurve.columns])
+
+    performance = traverseOutput(args).unstack().swaplevel(i=0, j=1, axis='columns').sort_index(1)
+
+    # result = pd.concat([trainCurve, performance], axis='columns')
+    # result = trainCurve.join(performance)
+    
+    if args.output is not None:
+        with pd.ExcelWriter(args.output) as writer:
+            trainCurve.to_excel(writer, sheet_name='Train')
+            performance.to_excel(writer, sheet_name='Validation')
+
     return
 
 if __name__ == "__main__":
-    args = argparse.ArgumentParser(prog="Experiment Tider")
-    args.add_argument("-d", "--dir", required=True, default="./output")
-    args.add_argument("-l", "--log", required=True, default="./log")
+    argparser = argparse.ArgumentParser(prog="Experiment Tider")
+    argparser.add_argument("-d", "--dir", default="./output")
+    argparser.add_argument("-l", "--log", default="./log")
+    argparser.add_argument("-o", "--output")
+    argparser.add_argument("-n", "--number", type=int, default=3)
+
+    args = argparser.parse_args()
     
     main(args)
